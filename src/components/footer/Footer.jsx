ec90e4b9109fb84419dbfc6c460bcc202fb278ab
Ken Christie <javawebken@gmail.com>
2026-01-13 17:22:31 -0500

    Added dark theme

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,23 +3,23 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center py-4 font-primary text-gray-700">
+    <footer className="flex justify-center items-center py-4 font-primary text-gray-700 dark:text-gray-300">
       Built with
-      <FontAwesomeIcon 
-        icon={faHeart} 
+      <FontAwesomeIcon
+        icon={faHeart}
         className="text-red-600 mx-1 animate-pulse"
         aria-hidden="true"
       />
       by
-      <a 
-        href="https://github.com/JavaWebKen/" 
-        target="_blank" 
+      <a
+        href="https://github.com/JavaWebKen/"
+        target="_blank"
         rel="noreferrer"
-        className="text-primary font-semibold px-1 transition-colors duration-300 hover:text-dark"  
+        className="text-primary dark:text-light font-semibold px-1 transition-colors duration-300 hover:text-dark dark:hover:text-lighter"
       >
         JavaWebKen
       </a>
-    </footer> 
+    </footer>
   );
 }
   
